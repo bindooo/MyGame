8599a29de1b39635a13cef5947460e405d34910f
bindooo <bindicsgeri@gmail.com>
2016-06-13 16:11:45 +0200

    Camera Test included

--- desktop/src/com/faveoffate/mygame/desktop/DesktopLauncher.java
+++ desktop/src/com/faveoffate/mygame/desktop/DesktopLauncher.java
@@ -4,14 +4,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.faveoffate.mygame.MyGame;
 import com.faveoffate.mygame.MyGameScreen;
+import com.faveoffate.mygame.OrthographicCameraExample;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "MyGame";
-		config.width = 480;
-		config.height = 800;
-		new LwjglApplication(new MyGame(), config);
+		config.width = 300;
+		config.height = 300;
+		new LwjglApplication(new OrthographicCameraExample(), config);
 
 	}
 }
